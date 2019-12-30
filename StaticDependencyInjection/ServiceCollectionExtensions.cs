d940a0cfaf0ec9033a365948640c2d031c17f609
Kohei Hakoishi <wipiano@gmail.com>
2019-12-31 01:35:51 +0900

    add to service collection

---
+++ StaticDependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace StaticDependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add services in the assembly that marked by *ServiceAttribute.
+        /// </summary>
+        public static IServiceCollection AddStaticServices(this IServiceCollection services, Assembly targetAssembly)
+        {
+            var targets = targetAssembly.GetTypes()
+                .Select(t => (type: t, attr: t.GetCustomAttributes().OfType<ServiceAttribute>().FirstOrDefault()));
+
+            foreach (var (type, attr) in targets)
+            {
+                var descriptor =
+                    ServiceDescriptor.Describe(attr.InterfaceType ?? type, attr.ImplType ?? type, attr.LifeTime);
+
+                if (attr.Weak)
+                {
+                    services.TryAdd(descriptor);
+                }
+                else
+                {
+                    services.Add(descriptor);
+                }
+            }
+
+            return services;
+        }
+    }
+}
